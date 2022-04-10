8ff258dbd5b2d995d05950a62dd4605cd8b1e9cb
yohanesadrian <s3764954@student.rmit.edu.au>
2022-04-10 20:45:52 +1000

    update

--- src/index.js
+++ src/index.js
@@ -1,43 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-
-
-import logger from 'redux-logger'
-import thunk from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-
-// redux 
-import { createStore,applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-
-import rootReducer from './reducers'
-
-import routes from './routes'
-import { BrowserRouter as Router, BrowserRouter, Route } from 'react-router-dom'
-
-import NavBar from './components/NavBar'
-import setAuthToken from './utils/setAuthToken'
-
-import { setCurrentUser } from './actions/LoginAction'
-import Decode from 'jwt-decode'
-
-const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(logger,thunk)))
-if(localStorage.jwToken){
-    setAuthToken(localStorage.jwToken);
-    store.dispatch(setCurrentUser(Decode(localStorage.jwToken)))
-}
-
-
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-    <Provider store = { store }>
-        <Router routes = { routes }>
-            <NavBar/>
-            { routes  }
-        </Router>
-    </Provider>
-    ,
-  document.getElementById('root')
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById("root")
 );
 
+serviceWorker.unregister();
